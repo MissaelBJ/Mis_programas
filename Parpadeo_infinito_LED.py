c0157977117f6aa48ee12fec8c81b7e7482a7bf6
MissaelBJ <ing_missael@hotmail.com>
2023-05-10 12:39:47 -0600

    Segundo programa parpadeo de led

---
+++ Parpadeo_infinito_LED.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(2,GPIO.HIGH)
+        time.sleep(0.4)
+        GPIO.output(2,GPIO.LOW)
+        time.sleep(0.2)
+except:
+    print("Fin del programa")
+    GPIO.cleaunp()
+    
