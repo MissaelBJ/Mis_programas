e697bb56afe0dd95c75645bb833cc28a6671b492
MissaelBJ <ing_missael@hotmail.com>
2023-05-10 16:02:23 -0600

    programa de un led pulsador prende led

---
+++ Led_con_pulsador.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+GPIO.setup(20,GPIO.IN)
+
+try:
+    while True:
+        if GPIO.input(20) == GPIO.HIGH:
+            GPIO.output(2, GPIO.HIGH)
+        else:
+            GPIO.output(20,GPIO.LOW)
+except:
+    GPIO.cleanup()
+    print("FIN del programa")
+    
