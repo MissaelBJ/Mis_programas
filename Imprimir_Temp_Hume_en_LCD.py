c63dd597323d76dcd27297427f7fe005d162e768
MissaelBJ <ing_missael@hotmail.com>
2023-05-12 11:53:04 -0600

    corrigiendo programa ya que al imprimir no podemos concaenar o sumar un flotante con string así que los valores los convertimos a string

--- Imprimir_Temp_Hume_en_LCD.py
+++ Imprimir_Temp_Hume_en_LCD.py
@@ -10,8 +10,8 @@ lcd= lcddriver.lcd()
 try:
     while True:
         h,t = dht.read_retry(sensor, 4)
-        lcd.lcd_display_string("T0"+t,1)
-        lcd.lcd_display_string("H="+h,2)
+        lcd.lcd_display_string("T0"+str(t),1)
+        lcd.lcd_display_string("H="+str(h),2)
         time.sleep(2)
         lcd.lcd_clear()
 except:
